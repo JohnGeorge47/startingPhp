ad56218aed03e5f204d4a3c8fc223a8effdd93da
John George <johngeorge.ctprice@gmail.com>
2018-09-07 14:11:18 +0530

    added router

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
 
-require "database/connection.php";
-require "database/queryBuilder.php";
-
+require "core/database/connection.php";
+require "core/database/queryBuilder.php";
+require 'core/Router.php';
 $pdo=Connection::make();
 return new QueryBuilder($pdo);
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-require "functions.php";
 
-$query=require "bootstrap.php";
+$query=require "core/bootstrap.php";
+
+
+$router=new Router;
+require "routes.php";
+
+require $router->direct();
